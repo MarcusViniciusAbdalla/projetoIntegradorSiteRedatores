4276229d9d410e1f8610866c00a7cd3ef428612e
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-14 20:17:23 -0300

    Update redatores.js

--- projetoIntegradorSiteRedatores/routes/redatores.js
+++ projetoIntegradorSiteRedatores/routes/redatores.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Rotas de Redatores');
-});
+const RedatoresController = require('../controllers/RedatoresController');
+
+router.get('/', RedatoresController.index);
 
 module.exports = router;
