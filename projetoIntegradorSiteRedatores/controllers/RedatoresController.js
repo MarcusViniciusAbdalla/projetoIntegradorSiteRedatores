f734e27af66ecb3b647a137310763afa6c3d44ac
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-14 20:50:09 -0300

    Update RedatoresController.js

--- projetoIntegradorSiteRedatores/controllers/RedatoresController.js
+++ projetoIntegradorSiteRedatores/controllers/RedatoresController.js
@@ -3,7 +3,7 @@ const RedatoresModel = require('../models/RedatoresModel');
 const RedatoresController = {
   index: (req, res) => {
     const redatores = RedatoresModel.obterInformacoes();
-    res.send(redatores);
+    res.render('redatores',{redatores:redatores});
   }
 }
 
